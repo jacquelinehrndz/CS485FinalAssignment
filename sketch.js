ec8f3537a90d1cb46e3be07fe2128919c1228fc8
Jacqueline Hernandez <herna1195@csusm.edu>
2024-05-13 08:20:52 -0700

    uploading sprites and images

--- sketch.js
+++ sketch.js
@@ -1,11 +1,31 @@
+//global variables
+let bk;
+let sprites;
+let numberofBoids = 5;
+
 //declaring flock (array)
 const flock = [];
 
+//resize function
+function windowResize()
+{
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+//preloading
+function preload()
+{
+    //loading json file
+    //sprites = loadJSON("sprite/Penguins/animationData.json");
+    //loading bk
+    bk = loadImage("imgs/0.png")
+}
+
 function setup()
 {
-    createCanvas(640, 360);
+    createCanvas(windowWidth, windowHeight);
     //creating a bunch of boids 
-    for(let i = 0; i < 3; i++)
+    for(let i = 0; i < numberofBoids; i++)
         {
             //adding boid to flock array
             flock.push(new Boid());
@@ -14,8 +34,14 @@ function setup()
 
 function draw()
 {
-    background(51);
-
+    background(bk);
+/*
+    for(let i = flock.length - 1; i >= 0; i--)
+        {
+            let boid = flock[i];
+            let spr
+        }
+*/
     //display flock
     for(let boid of flock)
     {
---
+++ sprite.js
@@ -0,0 +1,69 @@
+const idle_state = ["idle"]
+
+//total of 10 sprites
+class Sprite
+{
+    constructor(data, numberofBoids) {
+        this.data = data;
+        // boid array
+        this.boids = [];
+
+        //push new instances of boids into the array
+        for (let i = 0; i < numberofBoids; i++) {
+            this.boids.push(new Boid());
+        }
+        this.x = width / 2;
+        this.y = height / 2;
+        this.currentAnimation = 'idle'; // Initial animation state
+        this.loadedImgs = {}; 
+        this.currentFrame = 0; // Current frame number
+    }
+
+    preloadAnimations() {
+        // Preload images for each animation
+        for (let animation in this.data.TenderBud) {
+            let frames = this.data.TenderBud[animation].length;
+            this.loadedImgs[animation] = [];
+            for (let i = 0; i < frames; i++) {
+                let animationPath = 'sprite/Penguins/TenderBud/${animation}/${i}.png';
+                this.loadedImgs[animation].push(loadImage(animationPath));
+            }
+        }
+    }
+
+
+    updatePosition() {
+        for (let boid of this.boids) {
+            boid.flock(this.boids);
+            boid.update();
+            //boid.limitSpeed(); // Limit speed
+            boid.checkEdges(); // Check for boundary collisions
+
+            boid.show();
+
+            this.x = boid.position.x; 
+            this.y = boid.position.y; 
+        }
+
+
+    }
+
+    display() {
+        // Get the frames for the current animation
+        let frames = this.loadedImgs[this.currentAnimation];
+        // Calculate the current frame number based on the number of frames
+        this.currentFrame = (this.currentFrame + 1) % frames.length;
+        // Draw the current frame
+        image(frames[this.currentFrame], this.x, this.y);
+    }
+
+
+    changeAnimation(animation) {
+        this.currentAnimation = animation;
+        // Reset the current frame when animation changes
+        this.currentFrame = 0;
+    }
+    getImageData(){
+        return this.loadedImgs[this.currentAnimation][this.currentFrame]
+    }
+}
